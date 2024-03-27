81b851809471c87151d1509723856a57fed9992f
rexus.sinonjs <sardar.itagency@gmail.com>
2024-03-28 03:58:56 +0600

    derfgt
    ret

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'esrzbit' );
+define( 'DB_NAME', 'eesrzbit' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'I}4f{M/=5&5$4]m[U(w_Xdm-d:u6ytvR~(TO)4Hqt1>h #&qgoaz_Ok15{v&*8f-' );
-define( 'SECURE_AUTH_KEY',  'n$J]W7>f1O.gxjs$8gRT&I|Nq+qE`N=+@FnIQy[m:M>K#[!]:$l5j.A!4mR^1!YG' );
-define( 'LOGGED_IN_KEY',    '$F5:i)#1([3R`W}~B=.{f6#{Et%1i=oqIew/d=d%gmF$~!uW6JL&+DlW(oL(c->a' );
-define( 'NONCE_KEY',        '[cz8fUw~qF}A+4w+^v!F`!ZTh/*;Q%?j$PEsWOqfxUA:a=1I!(?i}wGMFf1[Uu%S' );
-define( 'AUTH_SALT',        'sAX~YD#PB+RQu[4/jn*-nKj.&Vitvl?v[zf}Ie#@cj@gI{mcR7tKMmT(Hy.AQpQr' );
-define( 'SECURE_AUTH_SALT', 'PJdHedA5dX04-OeY;8*Hn[JA>BKmKzY)R`69=Wq1_2c[m)N`Iee#0d-]cEV5MJ{T' );
-define( 'LOGGED_IN_SALT',   'EP^ryWm`G=VUmQ5L&x[0@&]o`$nX/Orn6nII6UFyq..L!I]=Dc.nkba((3,ZH&[t' );
-define( 'NONCE_SALT',       'i(++/X.B`64bf}f*jOA}qs]OxE3Oj@qm>bdt&P}*p#v^Y`?LFT/5@0DAcmrH5B6Z' );
+define( 'AUTH_KEY',         't@Q@dMWmeTnRNmT1gGaI1&IR}6kDz+1TA2_&xlf4KX#=vj+>we_Y25JE|vMLZ}D[' );
+define( 'SECURE_AUTH_KEY',  '4y:W4/fCLcRkLkHu!vj9%oE6k_oe)1!]O{Im1^1}1Y{Kp]o/PxpSO&pkEQRq0&}]' );
+define( 'LOGGED_IN_KEY',    '*Ubcilw^`m9cHn4I/(REnpz4mBY=Zl-=d=m8Bq{d6m[Y)mk;:B*S8>LJOoP<+NGG' );
+define( 'NONCE_KEY',        'pMjqlZ<:pBC7*%zrgGvdd3HLfw&Gat2daP&>}ZH6+D{G]NF5UKzQvUV80EQuseQ~' );
+define( 'AUTH_SALT',        '%LL-`/|HNFh73u0d8Y#b2k`eM]^#fw<K~09KDFS|k,ml$powcf~~mto}VIFY 1(/' );
+define( 'SECURE_AUTH_SALT', '21|O._r2.C$B/!Jfs <R!bbN;#{!2o>&}sQN zpI]Y65G&9{P/a1.5B(8aUC?]cj' );
+define( 'LOGGED_IN_SALT',   '*u>AYx5]!jLr)!0Krr}8!4z./p}}=+E*&DX;_`b%7K{#Fn[xuL6-]nv87/ :5=/=' );
+define( 'NONCE_SALT',       '.;GIs6yD2,8TmwiYNsQu.kw}|T07bw)e;JD+%7s>~*(}Wf4)p%CQ+^ O 28-{7_b' );
 
 /**#@-*/
 
